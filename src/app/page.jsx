0b990f145d46ba3ec62862c7e3edda4dc6619143
MBBrunito <tecnolastipas@gmail.com>
2023-10-24 19:45:34 -0300

    Solucionando cache de Vercel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,26 +2,45 @@ import "./Home.css";
 import Footer from "./components/Footer";
 import Dolares from "./components/dolares";
 
-async function dolares() {
-   const apiUrl = `${process.env.DOLARAPI}?timestamp=${Date.now()}`;
-   const requestOptions = {
-      method: "GET",
-      headers: {
-         "Cache-Control": "no-store, max-age=0", // Desactiva la caché
-      },
-   };
+// async function dolares() {
+//    const apiUrl = `${process.env.DOLARAPI}?timestamp=${Date.now()}`;
+//    const requestOptions = {
+//       method: "GET",
+//       headers: {
+//          "Cache-Control": "no-store", // Desactiva la caché
+//       },
+//    };
 
-   try {
-      const resp = await fetch(apiUrl, requestOptions);
-      const data = await resp.json();
-      return data;
-   } catch (error) {
-      console.error("Error al obtener datos del API de Dólar:", error);
-      throw error;
-   }
-}
+//    try {
+//       const resp = await fetch(apiUrl, requestOptions);
+//       const data = await resp.json();
+//       return data;
+//    } catch (error) {
+//       console.error("Error al obtener datos del API de Dólar:", error);
+//       throw error;
+//    }
+// }
 
 async function HomePage() {
+   async function dolares() {
+      const apiUrl = `${process.env.DOLARAPI}?timestamp=${Date.now()}`;
+      const requestOptions = {
+         method: "GET",
+         headers: {
+            "Cache-Control": "no-store", // Desactiva la caché
+         },
+      };
+
+      try {
+         const resp = await fetch(apiUrl, requestOptions);
+         const data = await resp.json();
+         return data;
+      } catch (error) {
+         console.error("Error al obtener datos del API de Dólar:", error);
+         throw error;
+      }
+   }
+
    const valores = await dolares();
    console.log(valores);
 
